77a83a6ee52487915927d2946783c5da8bc29f91
Baglaev Kirill <baglaevkirill@gmail.com>
2026-04-09 23:27:19 +0300

    setup model custom_user

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,8 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -33,6 +35,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'photo_web',
+    'imagekit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'photo_web.User'
--- photo_web/models.py
+++ photo_web/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import FileExtensionValidator
 
-# Create your models here.
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill, ResizeToFit
+
+
+def user_avatar_path(instance, filename):
+    # instance — это объект User, filename — исходное имя файла
+    return f'avatars/user_{instance.id}/{filename}'
+
+class User(AbstractUser):
+    #Флаг - "Является ли пользователь модератором"
+    is_moderator = models.BooleanField(
+        #По умолчанию роль модератора не присвоена
+        default=False
+        )
+    
+    #Оригинал аватара хранимый в БД, как путь к файлу
+    avatar = models.ImageField(
+        upload_to=user_avatar_path,
+        null=True,
+        blank=True,
+        #Допустимые расширения файлов изображений в аватаре
+        validators=[FileExtensionValidator(['jpg','jpeg','png','webp'])]
+        )
+    
+    #Версии изображения - виртуальные поля модели, не являются полями БД
+    #Версия аватара для отображения в комментариях / в шапке сайта
+    avatar_small = ImageSpecField(
+        source='avatar',
+        #ResizeToFill - обрезает до указанных размеров (от центра)
+        processors=[ResizeToFill(50,50)],
+        format='JPEG',
+        options={'quality':80}
+        )
+    #Версия аватара для отображения в профиле
+    avatar_profile = ImageSpecField(
+        source='avatar',
+        #ResizeToFit - масштабирует сохраняя пропорции
+        processors=[ResizeToFit(150,150)],
+        format='JPEG',
+        options={'quality':85}
+    )
